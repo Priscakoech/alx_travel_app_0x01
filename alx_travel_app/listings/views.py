74c55eaba51adde1d19ca3e04748b7fdeba17af4
Priscakoech <priscakoech448@gmail.com>
2025-08-06 13:59:59 +0300

    added views and routers

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+#create viewsets for Listing and Booking using Django REST framework’s ModelViewSet.
+#Ensure that these views provide CRUD operations for both models.from django.shortcuts import render
+from .models import Listing, Booking
+from rest_framework import viewsets
+from .serializers import ListingSerializer, BookingSerializer.
+
+# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
